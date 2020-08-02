17c412cad06f95de5ef816c9b0056faeb0b8aead
Kathreen Fenchenko <fev3@mail.ru>
2020-08-02 12:23:36 +0300

    RWD done

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 (function(){
     var nav = document.querySelector('.header__menu');
+    var hamburger = document.querySelector('.hamburger');
+    var menuItem = document.querySelectorAll('.header__menu-item');
+    
+    
     if(nav) {
             nav.addEventListener('click',function(e) {
             var target = e.target;
@@ -8,6 +12,18 @@
             }
             e.preventDefault();
             kathleenFen.navigation.toggleToActiveLink(target);
-        });
+            });
+    };
+
+    if(hamburger) {
+        hamburger.addEventListener('click', function(e) {
+            hamburger.classList.toggle('hamburger_active');
+            nav.classList.toggle('header__menu_active');
+        })
+    };
+    if(menuItem) {
+       console.log(menuItem)  
     }
+
+
 }())
